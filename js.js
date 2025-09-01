534b8cfb1b68bf9631fe1d3eacb0cbf2b3f52903
Sooqyip <192369409+MelboucyMalak@users.noreply.github.com>
2025-09-01 16:30:15 +0100

    Add basic ui to the game

--- js.js
+++ js.js
@@ -1,61 +1,61 @@
 function getComputerChoice(){
    let random= Math.floor(Math.random()*10);
-   let ComputerChoice= random>6 ? "ROCK": random>3 ? "PAPER": "SCISSORS";
-   console.log("computer: "+ComputerChoice)
-   return ComputerChoice
+   let computerChoice= random>6 ? "ROCK": random>3 ? "PAPER": "SCISSORS";
+   console.log("computer: "+computerChoice)
+   return computerChoice
 }
 
+const resultDiv=document.querySelector(".result")
+const scoreDiv=document.querySelector(".score")
 
-function getHumanChoice(){
-    let input=prompt("Enter Rock, Paper or Scissors").toUpperCase();
-    console.log("You: "+input)
-    return input
-}
-
-
-function playRound(){
-    let computerChoice=getComputerChoice()
-    let humanChoice=getHumanChoice()
-    if(computerChoice==humanChoice){return "equality"}
-    switch(humanChoice){
-        case "ROCK":
-            if(computerChoice=="SCISSORS"){
-                HumanScore++
-                console.log("You win")
-            }else{ComputerScore++ 
-                console.log("You loose")
-            }
-            break;
-        case "SCISSORS":
-            if(computerChoice=="PAPER"){
-                HumanScore++
-                console.log("You win")
-            }else{
-                ComputerScore++ 
-                console.log("You loose")
-            }
-            break;
-        case "PAPER":
-            if(computerChoice=="ROCK"){
-                HumanScore++
-                console.log("You win")
-            }else{
-                ComputerScore++ 
-                console.log("You loose")
-            }
-            break;
-    }
-}
 
 let HumanScore=0
 let ComputerScore=0
-function playGame(){
-    for(let i=1;i<=5;i++){
-        playRound()
-        console.log("Your score: "+HumanScore)
-        console.log("Computer's score: "+ComputerScore)
-    }
-}
+let result=''
+ 
+const btns=document.querySelectorAll("button")
+btns.forEach(btn=>
+    {btn.addEventListener("click",function(e){
+     if (HumanScore === 5 || ComputerScore === 5) {
+            let winner=(HumanScore==5)?"You are": "Computer is"
+            resultDiv.innerText = `${winner} the winner. Refresh to play again.`;
+            return;
+        }
+    let computerChoice=getComputerChoice()
+    let humanChoice=e.target.textContent
+    console.log("you: "+humanChoice)
+    if(computerChoice==humanChoice){result="equality"
+    }else{
+        switch(humanChoice){
+            case "ROCK":
+                if(computerChoice=="SCISSORS"){
+                    HumanScore++
+                    result="You win"
+                }else{ComputerScore++ 
+                    result="You loose"
+                }
+                break;
+            case "SCISSORS":
+                if(computerChoice=="PAPER"){
+                    HumanScore++
+                    result="You win"
+                }else{
+                    ComputerScore++ 
+                    result="You loose"
+                }
+                break;
+            case "PAPER":
+                if(computerChoice=="ROCK"){
+                    HumanScore++
+                    result="You win"
+                }else{
+                    ComputerScore++ 
+                    result="You loose"
+                }
+                break;
+        }}
+    resultDiv.innerText=result
+    scoreDiv.innerText=`Your score: ${HumanScore} | Computer's score:${ComputerScore}` 
+})})
 
-playGame()
 
